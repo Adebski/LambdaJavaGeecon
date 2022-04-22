98d14e77484902632296a4ea7c35a128e1643a98
addebski <addebski>
2022-04-22 22:08:09 +0100

    Add package-and-deploy.sh script.

---
+++ package-and-deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace    # expands variables and prints a little + sign before the line
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
+mvn package
+
+cd cdk
+cdk synth && cdk deploy
